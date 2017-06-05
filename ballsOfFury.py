a69850f29e19bc393243e18aed7b8dfcfb1461b3
Arthur Brum <arthur@example.com>
2017-06-05 20:44:49 -0300

    Initial commit

---
+++ ballsOfFury.py
@@ -0,0 +1,155 @@
+'''
+	ballsOfFury.py
+
+	Created on: 31/05/2017
+		Author: Arthur Brum (RA: 157701)
+
+	Simples plot e movimentacao de poligonos com base em tempo
+
+'''
+
+from __future__ import division
+from __future__ import print_function
+
+import sys
+import math
+import numpy as np
+
+import OpenGL.GL as gl
+import OpenGL.GLU as glu
+import OpenGL.GLUT as glut
+
+from polygonsHandler import PolygonsHandler
+
+def init():
+    gl.glClearColor(0.0, 0.0, 0.0, 1.0)
+    gl.glShadeModel(gl.GL_FLAT)
+
+
+def display():
+    global cameraX, cameraY, cameraZ
+    global p
+    global oldTimeSinceStart
+    gl.glClear(gl.GL_COLOR_BUFFER_BIT)
+
+    timeSinceStart = glut.glutGet(glut.GLUT_ELAPSED_TIME)
+    deltaTime = timeSinceStart - oldTimeSinceStart
+    oldTimeSinceStart = timeSinceStart
+
+    gl.glPushMatrix()
+
+    # Configura posicao da camera
+    gl.glRotatef(cameraX, 1.0, 0.0, 0.0)
+    gl.glRotatef(cameraY, 0.0, 1.0, 0.0)
+    gl.glRotatef(cameraZ, 0.0, 0.0, 1.0)
+
+
+    #Operations to change position of all polygons (time based movement)
+    p.pos += p.vel/10000 *deltaTime
+
+    for i in range(p.size):
+        gl.glPushMatrix()
+
+        gl.glTranslatef(p.pos[i][0], p.pos[i][1], 0)
+        gl.glColor3f(p.color[i][0], p.color[i][1], p.color[i][2])
+        drawCircle(0.1)
+
+        gl.glPopMatrix()
+
+
+    gl.glPopMatrix()
+
+    glut.glutSwapBuffers()
+
+
+def reshape(w, h):
+    gl.glViewport(0, 0, w, h)
+    gl.glMatrixMode(gl.GL_PROJECTION)
+    gl.glLoadIdentity()
+    glu.gluPerspective(60.0, w / h, 1.0, 20.0)
+    gl.glMatrixMode(gl.GL_MODELVIEW)
+    gl.glLoadIdentity()
+    glu.gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
+
+def keyboard(key, x, y):
+    global cameraX, cameraY, cameraZ
+
+    # Alteracoes na camera
+    if key == 's':
+        cameraX = (cameraX + 5) % 360
+    elif key == 'w':
+        cameraX = (cameraX - 5) % 360
+    elif key == 'd':
+        cameraY = (cameraY + 5) % 360
+    elif key == 'a':
+        cameraY = (cameraY - 5) % 360
+    elif key == 'q':
+        cameraZ = (cameraZ + 5) % 360
+    elif key == 'e':
+        cameraZ = (cameraZ - 5) % 360
+
+    else:
+        return
+
+    glut.glutPostRedisplay()
+
+
+def generateCirclePoints():
+    global nPoints, points, alreadyGenerated
+
+    for val in np.linspace(0, 2*math.pi, num=nPoints, endpoint = False):
+        points.append([math.cos(val), math.sin(val)])
+
+    alreadyGenerated = nPoints
+
+def drawCircle(radius):
+    global nPoints, points, alreadyGenerated
+
+    if alreadyGenerated != nPoints:
+        generateCirclePoints()
+
+    gl.glBegin(gl.GL_POLYGON)
+    for i in range(nPoints):
+        gl.glVertex3f(points[i][0]*radius, points[i][1]*radius, 0.0)
+    gl.glEnd()
+
+
+def main():
+    _ = glut.glutInit(sys.argv)
+    glut.glutInitDisplayMode(glut.GLUT_DOUBLE | glut.GLUT_RGB)
+
+    glut.glutInitWindowSize(700, 700)
+    glut.glutInitWindowPosition(100, 100)
+    _ = glut.glutCreateWindow(sys.argv[0])
+
+    init()
+
+    global cameraX, cameraY, cameraZ
+    cameraX = 0
+    cameraY = 0
+    cameraZ = 0
+
+    _ = glut.glutDisplayFunc(display)
+    _ = glut.glutReshapeFunc(reshape)
+    _ = glut.glutKeyboardFunc(keyboard)
+    _ = glut.glutIdleFunc(display)
+
+    global p
+    p = PolygonsHandler()
+    p.add_polygon(-1,0,1,0.5, '#00aa00', 'c1')
+    p.add_polygon(1,1,0,0, '#aa0000', 'c2')
+    p.add_polygon(1,-1,0,0, '#0099FF', 'c3')
+
+
+    global nPoints, points, alreadyGenerated, oldTimeSinceStart
+    alreadyGenerated = 0
+    nPoints = 12
+    points = []
+
+    oldTimeSinceStart = 0
+
+    glut.glutMainLoop()
+
+
+if __name__ == "__main__":
+    main()
---
+++ polygonsHandler.py
@@ -0,0 +1,38 @@
+'''
+	polygonsHandler.py
+
+	Created on: 31/05/2017
+		Author: Arthur Brum (RA: 157701)
+
+    Classe para controle de todos os poligonos e realizacao de operacoes
+
+'''
+
+import numpy as np
+import matplotlib.colors as colors
+
+class PolygonsHandler():
+
+    def __init__(self):
+        self.size = 0
+        self.id = {}                            # Dictionary de ids  {string:int}
+        self.pos = np.empty([0,2])              # Array Numpy de posicoes
+        self.vel = np.empty([0,2])              # Array Numpy de velocidades
+        self.color = []                         # Lista de cores - tupla (R,G,B)
+        #radius
+        #typeOfPolygon
+        
+    def add_polygon(self, posX, posY, velX, velY, color='#ffffff', id=None):
+
+        self.pos = np.vstack((self.pos, [posX, posY]))
+        self.vel = np.vstack((self.vel, [velX, velY]))
+        self.color.append(colors.hex2color(color))
+        #radius
+        #typeOfPolygon
+
+        if(id == None):
+            self.id[str(self.size)] = self.size
+        else:
+            self.id[str(id)] = self.size
+
+        self.size += 1
